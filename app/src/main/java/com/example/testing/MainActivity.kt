da32615bfe7c195b62b4e975f55cf4c26d218651
fauzanashshidiq <muhzeen07@gmail.com>
2025-09-05 05:03:08 +0700

    first commit

---
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -0,0 +1,33 @@
+//fun main() {
+//    println("Hello, world!")
+//}
+
+//fun main() {
+//    val popcorn = 5
+//    val hotdog = 7
+//    println(popcorn)
+//    println(hotdog)
+//    val customers = 10
+//    println("There are $customers customers")
+//    println("There are ${customers + 1} customers")
+//}
+
+//fun main() {
+//    var d: Int
+//    d = 3
+//    val e: String = "hello"
+//    d = d + 5
+//    d += 7
+//    d -= 3
+//    d *= 2
+//    d /= 3
+//    println(d)
+//    println(e)
+//}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+fun main() {
+    println(sum(1, 2))
+}
