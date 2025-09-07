076cd106e551048f9842ab4b7bd4d1e617427a9b
fauzanashshidiq <muhzeen07@gmail.com>
2025-09-07 23:53:29 +0700

    Class

--- app/src/main/java/com/example/testing/MainActivity.kt
+++ app/src/main/java/com/example/testing/MainActivity.kt
@@ -40,9 +40,24 @@
 //    // HELLO
 //}
 
+//fun main() {
+//    val upperCaseString = { text: String ->
+//        text.uppercase() }
+//    println(upperCaseString("hello"))
+//    // HELLO
+//}
+
+class Contact(val id: Int, var email: String)
 fun main() {
-    val upperCaseString = { text: String ->
-        text.uppercase() }
-    println(upperCaseString("hello"))
-    // HELLO
+    val contact = Contact(1, "mary@gmail.com")
+
+    // Prints the value of the property: email
+    println(contact.email)
+    // mary@gmail.com
+    // Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+    // Prints the new value of the property: email
+    println(contact.email)
+    // jane@gmail.com
 }
